7c12d7b0aa245ebf9e93069131e33af692bcf3e1
adyshkiss@outlook.com <adyshkiss@outlook.com>
2020-10-17 22:59:31 +0300

    Создана страница авторизации

---
+++ FishingShop/ProductWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FishingShop
+{
+    /// <summary>
+    /// Логика взаимодействия для ProductWindow.xaml
+    /// </summary>
+    public partial class ProductWindow : Window
+    {
+        public ProductWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn1_Selected(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Главная");
+        }
+
+        private void btn2_Selected(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("1");
+        }
+
+        private void btn3_Selected(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("2");
+        }
+
+        
+    }
+}
